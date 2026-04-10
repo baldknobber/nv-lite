4e494547ec73b6b04f981c6bd189275b9d88e4d3
baldknobber <baldknobber@users.noreply.github.com>
2026-04-09 23:10:39 -0500

    v0.7.0-alpha: About page, window persistence

--- src/NVLite.App/MainWindow.xaml.cs
+++ src/NVLite.App/MainWindow.xaml.cs
@@ -14,10 +14,34 @@ public sealed partial class MainWindow : Window
         ExtendsContentIntoTitleBar = true;
         SetTitleBar(AppTitleBar);
         AppWindow.SetIcon("Assets/AppIcon.ico");
-        AppWindow.Resize(new SizeInt32(1024, 700));
+
+        RestoreWindowState();
 
         // Select Dashboard by default
         NavView.SelectedItem = NavView.MenuItems[0];
+
+        Closed += MainWindow_Closed;
+    }
+
+    private void RestoreWindowState()
+    {
+        var s = App.Settings.Settings;
+        var w = s.WindowWidth > 200 ? (int)s.WindowWidth : 1024;
+        var h = s.WindowHeight > 200 ? (int)s.WindowHeight : 700;
+        AppWindow.Resize(new SizeInt32(w, h));
+
+        if (s.WindowX >= 0 && s.WindowY >= 0)
+            AppWindow.Move(new PointInt32(s.WindowX, s.WindowY));
+    }
+
+    private void MainWindow_Closed(object sender, WindowEventArgs args)
+    {
+        var s = App.Settings.Settings;
+        s.WindowWidth = AppWindow.Size.Width;
+        s.WindowHeight = AppWindow.Size.Height;
+        s.WindowX = AppWindow.Position.X;
+        s.WindowY = AppWindow.Position.Y;
+        App.Settings.Save();
     }
 
     private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
--- src/NVLite.App/Views/AboutPage.xaml.cs
+++ src/NVLite.App/Views/AboutPage.xaml.cs
@@ -1,9 +1,18 @@
+using System.Reflection;
 using Microsoft.UI.Xaml.Controls;
 
 namespace NVLite.App.Views;
 
 public sealed partial class AboutPage : Page
 {
+    public string AppVersion { get; } =
+        Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+        ?? Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+        ?? "unknown";
+
+    public string RuntimeInfo { get; } =
+        $".NET {Environment.Version} | {Environment.OSVersion}";
+
     public AboutPage()
     {
         InitializeComponent();
